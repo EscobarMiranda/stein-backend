a98334761b260d1d9f3cf41ac6851b62c9fb77ef
Kevin Escobar Miranda <info@EscobarMiranda.com>
2016-08-03 16:06:56 -0600

    basic structure of project

---
+++ API/Clients.svc.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace API
+{
+    public class Clients : IClients
+    {
+        DataClassesDataContext dataClasses = new DataClassesDataContext();
+
+        public bool Create(Client client)
+        {
+            try
+            {
+                dataClasses.Clients.InsertOnSubmit(client);
+                dataClasses.SubmitChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Client get(int id)
+        {
+            try
+            {
+                return (from client in dataClasses.Clients
+                        where client.idClient == id
+                        select client).Single();
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public List<Client> getList()
+        {
+            try
+            {
+                return (from clients in dataClasses.Clients
+                        where clients.status == true
+                        select clients).ToList();
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public bool Update(Client client)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ API/IClients.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace API
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IClients" in both code and config file together.
+    [ServiceContract]
+    public interface IClients
+    {
+        [OperationContract]
+        bool Create(Client client);
+
+        [OperationContract]
+        Client get(int id);
+
+        [OperationContract]
+        bool Update(Client client);
+
+        [OperationContract]
+        bool Delete(int id);
+
+        [OperationContract]
+        List<Client> getList();
+    }
+}
