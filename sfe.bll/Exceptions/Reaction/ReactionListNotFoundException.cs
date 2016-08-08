6860a2bbb12ac08e84fc65696fa5f0e1a8e15d71
Kevin Escobar Miranda <info@EscobarMiranda.com>
2016-08-08 15:46:59 -0600

    Get reaction list method created in reaction service, and unit test created

---
+++ sfe.bll/Exceptions/Reaction/ReactionListNotFoundException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace sfe.bll
+{
+    [Serializable]
+    internal class ReactionListNotFoundException : Exception
+    {
+        public ReactionListNotFoundException()
+        {
+        }
+
+        public ReactionListNotFoundException(string message) : base(message)
+        {
+        }
+
+        public ReactionListNotFoundException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected ReactionListNotFoundException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
