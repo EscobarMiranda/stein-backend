197fcbad52c59cb47e51ec63ed882ea2ffe140ce
Kevin Escobar Miranda <info@EscobarMiranda.com>
2016-08-08 16:25:55 -0600

    fix conflict

--- sfe.bll/VisitsLogic.cs
+++ sfe.bll/VisitsLogic.cs
@@ -1,11 +1,8 @@
-﻿using sfe.bll.Exceptions;
-using sfe.dal;
+﻿using sfe.dal;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace sfe.bll
 {
