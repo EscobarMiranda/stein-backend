5adb5f2837a86f2bde8fcc4eb709c7ca87aa3069
Kevin Escobar Miranda <info@EscobarMiranda.com>
2016-08-09 10:51:59 -0600

    create product logic and custom exceptions

---
+++ sfe.bll/ProductListNotFoundException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace sfe.bll
+{
+    [Serializable]
+    internal class ProductListNotFoundException : Exception
+    {
+        public ProductListNotFoundException()
+        {
+        }
+
+        public ProductListNotFoundException(string message) : base(message)
+        {
+        }
+
+        public ProductListNotFoundException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected ProductListNotFoundException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
