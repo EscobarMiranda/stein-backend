261174c3b3322dbfd196850be4d9590c591bbbc3
Luis Serrano <lserrano@pernix-solutions.com>
2016-08-08 23:06:57 -0600

    add correct files and correct structure

---
+++ sfe.api/Controllers/AgentController.cs
@@ -0,0 +1,41 @@
+﻿using sfe.dal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace sfe.api.Controllers
+{
+    public class AgentController : ApiController
+    {
+        // GET api/<controller>
+        public IEnumerable<Agent> Get()
+        {
+            return bll.AgentLogic.Get();
+        }
+
+        // GET api/<controller>/5
+        public Agent Get(int id)
+        {
+            return bll.AgentLogic.Get(id);
+        }
+
+        // POST api/<controller>
+        public void Post(Agent client)
+        {
+            bll.AgentLogic.Post(client);
+        }
+
+        // PUT api/<controller>/5
+        public void Put(Agent client)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
