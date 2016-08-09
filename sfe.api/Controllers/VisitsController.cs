dc4f4efeaebd4b95c8d38344d997a7ea81a4b5aa
Kevin Escobar Miranda <info@EscobarMiranda.com>
2016-08-09 10:06:31 -0600

    delete unused api controller and modify visit api controller

--- sfe.api/Controllers/VisitsController.cs
+++ sfe.api/Controllers/VisitsController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using sfe.dal;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -10,30 +11,21 @@ namespace sfe.api.Controllers
     public class VisitsController : ApiController
     {
         // GET api/<controller>
-        public IEnumerable<string> Get()
+        public IEnumerable<Visit> Get()
         {
-            return new string[] { "value1", "value2" };
+            return bll.VisitsLogic.Get();
         }
 
         // GET api/<controller>/5
-        public string Get(int id)
+        public Visit Get(int id)
         {
-            return "value";
+            return bll.VisitsLogic.Get(id);
         }
 
         // POST api/<controller>
-        public void Post([FromBody]string value)
+        public void Post(Visit visit)
         {
-        }
-
-        // PUT api/<controller>/5
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/<controller>/5
-        public void Delete(int id)
-        {
+            bll.VisitsLogic.Post(visit);
         }
     }
 }
