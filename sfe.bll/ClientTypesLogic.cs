d958b3884ebcbb14181b955efba8a0ba519d52ca
Kevin Escobar Miranda <info@EscobarMiranda.com>
2016-08-23 16:25:49 -0600

    Create client types service

---
+++ sfe.bll/ClientTypesLogic.cs
@@ -0,0 +1,29 @@
+﻿using sfe.bll.Exceptions;
+using sfe.dal;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sfe.bll
+{
+    public class ClientTypesLogic
+    {
+        private static DataClassesDataContext db = Database.Instance;
+        public static List<ClientType> Read()
+        {
+            try
+            {
+                return (from types in db.ClientTypes
+                        select types).ToList();
+            }
+            catch (Exception e)
+            {
+                EventLog.WriteEntry("sfe", e.StackTrace.ToString(), EventLogEntryType.Error);
+                throw new ClientTypesListNotFoundException("Client Types list not found");
+            }
+        }
+    }
+}
---
+++ sfe.bll/Exceptions/ClientTypesListNotFoundException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace sfe.bll.Exceptions
+{
+    [Serializable]
+    internal class ClientTypesListNotFoundException : Exception
+    {
+        public ClientTypesListNotFoundException()
+        {
+        }
+
+        public ClientTypesListNotFoundException(string message) : base(message)
+        {
+        }
+
+        public ClientTypesListNotFoundException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected ClientTypesListNotFoundException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
