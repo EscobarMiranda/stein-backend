1295c05949a672dfac31a64d5d85c56ed4ff432f
Kevin Escobar Miranda <info@EscobarMiranda.com>
2016-08-23 10:58:21 -0600

    Enable cors to allow any origin

--- sfe.api/Controllers/ProductController.cs
+++ sfe.api/Controllers/ProductController.cs
@@ -5,9 +5,12 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace sfe.api.Controllers
 {
+    //TODO: change origin to app url
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
     public class ProductController : ApiController
     {
         // GET api/<controller>
