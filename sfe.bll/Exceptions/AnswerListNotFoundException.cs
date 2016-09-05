d7f93ef73edabced435c0f80d9e276302e811c66
Kevin Escobar Miranda <info@EscobarMiranda.com>
2016-09-05 13:56:09 -0600

    Create logic, controller and test for answers

---
+++ sfe.bll/Exceptions/AnswerListNotFoundException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace sfe.bll.Exceptions
+{
+    [Serializable]
+    internal class AnswerListNotFoundException : Exception
+    {
+        public AnswerListNotFoundException()
+        {
+        }
+
+        public AnswerListNotFoundException(string message) : base(message)
+        {
+        }
+
+        public AnswerListNotFoundException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected AnswerListNotFoundException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
---
+++ sfe.test/UnitTestAnswer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using sfe.dal;
+using System.Collections.Generic;
+
+namespace sfe.test
+{
+    [TestClass]
+    public class UnitTestAnswer
+    {
+        [TestMethod]
+        public void TestMethodCreateAnswers()
+        {
+            int quantity = bll.AnswerLogic.Read().Count;
+            List<Answer> answers = new List<Answer>() {
+                                                        new Answer() { FK_client = 1, FK_option = 1 },
+                                                        new Answer() { FK_client = 1, FK_option = 2 }
+                                                      };
+            bll.AnswerLogic.Create(answers);
+            Assert.IsTrue(bll.AnswerLogic.Read().Count > quantity + 1);
+        }
+
+        [TestMethod]
+        public void TestMethodReadQuestions()
+        {
+            Assert.IsTrue(bll.AnswerLogic.Read().Count > 0);
+        }
+    }
+}
