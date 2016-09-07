e00a6e350c4a2a329302483c8bd4c72577a981a5
Kevin Escobar Miranda <info@EscobarMiranda.com>
2016-09-07 10:03:43 -0600

    Add date column in Answers table and update service

--- CreateDatabase.sql
+++ CreateDatabase.sql
@@ -1,7 +1,7 @@
 CREATE TABLE Adoptions (idAdoption int IDENTITY(1, 1) NOT NULL, name varchar(50) NOT NULL, PRIMARY KEY (idAdoption));
-CREATE TABLE Answers (idAnswer int IDENTITY(1, 1) NOT NULL, FK_option int NOT NULL, FK_client int NOT NULL, PRIMARY KEY (idAnswer));
+CREATE TABLE Answers (idAnswer int IDENTITY(1, 1) NOT NULL, [date] datetime DEFAULT GETDATE() NOT NULL, FK_option int NOT NULL, FK_client int NOT NULL, PRIMARY KEY (idAnswer));
 CREATE TABLE Clients (idClient int IDENTITY(1, 1) NOT NULL, name varchar(50) NOT NULL, lastName varchar(50) NULL, creationDate datetime DEFAULT GETDATE() NOT NULL, speciality varchar(50) NULL, country varchar(50) NOT NULL, province varchar(100) NULL, address1 varchar(400) NULL, address2 varchar(400) NULL, zone varchar(50) NULL, latitude float(10) NULL, longitude float(10) NULL, email varchar(50) NOT NULL, phone1 varchar(20) NULL, phone2 varchar(20) NULL, active bit DEFAULT 1 NOT NULL, maxNumVisits int NOT NULL, FK_adoption int NOT NULL, FK_potential int NOT NULL, FK_clientType int NOT NULL, FK_user int NOT NULL, PRIMARY KEY (idClient));
-CREATE TABLE ClientTypes (idClientType int IDENTITY(1, 1) NOT NULL, name varchar(50) NOT NULL, PRIMARY KEY (idClientType));
+CREATE TABLE ClientTypes (idClientType int IDENTITY(1, 1) NOT NULL, name varchar(50) NOT NULL, hasProducts bit NOT NULL, PRIMARY KEY (idClientType));
 CREATE TABLE Options (idOption int IDENTITY(1, 1) NOT NULL, description varchar(300) NOT NULL, FK_question int NOT NULL, active bit DEFAULT 1 NOT NULL, PRIMARY KEY (idOption));
 CREATE TABLE Potentials (idPotential int IDENTITY(1, 1) NOT NULL, name varchar(50) NOT NULL, PRIMARY KEY (idPotential));
 CREATE TABLE Products (idProduct int IDENTITY(1, 1) NOT NULL, name varchar(100) NOT NULL, active bit DEFAULT 1 NOT NULL, PRIMARY KEY (idProduct));
@@ -12,12 +12,6 @@ CREATE TABLE Users (idUser int IDENTITY(1, 1) NOT NULL, name varchar(50) NOT NUL
 CREATE TABLE UserTypes (idAgentType int IDENTITY(1, 1) NOT NULL, name varchar(50) NOT NULL, PRIMARY KEY (idAgentType));
 CREATE TABLE Visits (idVisit int IDENTITY(1, 1) NOT NULL, FK_client int NOT NULL, FK_reaction int NOT NULL, comment varchar(400) NOT NULL, [date] datetime NOT NULL, latitude float(10) NULL, longitude float(10) NULL, FK_visitType int NOT NULL, PRIMARY KEY (idVisit));
 CREATE TABLE VisitTypes (idVisitType int IDENTITY(1, 1) NOT NULL, name varchar(50) NOT NULL, PRIMARY KEY (idVisitType));
-ALTER TABLE Clients ADD CONSTRAINT FKClients270038 FOREIGN KEY (FK_potential) REFERENCES Potentials (idPotential);
-ALTER TABLE Clients ADD CONSTRAINT FKClients619803 FOREIGN KEY (FK_adoption) REFERENCES Adoptions (idAdoption);
-ALTER TABLE Visits ADD CONSTRAINT FKVisits976056 FOREIGN KEY (FK_visitType) REFERENCES VisitTypes (idVisitType);
-ALTER TABLE Answers ADD CONSTRAINT FKAnswers50411 FOREIGN KEY (FK_client) REFERENCES Clients (idClient);
-ALTER TABLE Answers ADD CONSTRAINT FKAnswers8255 FOREIGN KEY (FK_option) REFERENCES Options (idOption);
-ALTER TABLE Options ADD CONSTRAINT FKOptions244805 FOREIGN KEY (FK_question) REFERENCES Questions (idQuestion);
 ALTER TABLE Visits ADD CONSTRAINT FKVisits304121 FOREIGN KEY (FK_client) REFERENCES Clients (idClient);
 ALTER TABLE Visits ADD CONSTRAINT FKVisits336297 FOREIGN KEY (FK_reaction) REFERENCES Reactions (idReaction);
 ALTER TABLE Clients ADD CONSTRAINT FKClients629649 FOREIGN KEY (FK_clientType) REFERENCES ClientTypes (idClientType);
@@ -25,3 +19,9 @@ ALTER TABLE Users ADD CONSTRAINT FKUsers503235 FOREIGN KEY (FK_userType) REFEREN
 ALTER TABLE ProductsPerVisit ADD CONSTRAINT FKProductsPe486417 FOREIGN KEY (FK_visit) REFERENCES Visits (idVisit);
 ALTER TABLE ProductsPerVisit ADD CONSTRAINT FKProductsPe587229 FOREIGN KEY (FK_product) REFERENCES Products (idProduct);
 ALTER TABLE Clients ADD CONSTRAINT FKClients628131 FOREIGN KEY (FK_user) REFERENCES Users (idUser);
+ALTER TABLE Clients ADD CONSTRAINT FKClients270038 FOREIGN KEY (FK_potential) REFERENCES Potentials (idPotential);
+ALTER TABLE Clients ADD CONSTRAINT FKClients619803 FOREIGN KEY (FK_adoption) REFERENCES Adoptions (idAdoption);
+ALTER TABLE Visits ADD CONSTRAINT FKVisits976056 FOREIGN KEY (FK_visitType) REFERENCES VisitTypes (idVisitType);
+ALTER TABLE Answers ADD CONSTRAINT FKAnswers50411 FOREIGN KEY (FK_client) REFERENCES Clients (idClient);
+ALTER TABLE Answers ADD CONSTRAINT FKAnswers8255 FOREIGN KEY (FK_option) REFERENCES Options (idOption);
+ALTER TABLE Options ADD CONSTRAINT FKOptions244805 FOREIGN KEY (FK_question) REFERENCES Questions (idQuestion);
--- TestScript.sql
+++ TestScript.sql
@@ -3,7 +3,7 @@ insert into UserTypes (name) values ('Administrador'), ('Agente');
 insert into Users (name, lastName, email, username, password, creationDate, country, active, FK_userType)
 	values ('Maria', 'Rojas', 'mrojas@test.com', 'mrojas', 'Pernix123.', GETDATE(), 'Costa Rica', 1, 1),
 		   ('Juan', 'Perez', 'jperez@test.com', 'jperez', 'Pernix123.', GETDATE(), 'Costa Rica', 1, 2);
-insert into ClientTypes (name) values ('Doctor'), ('Farmacia'), ('Doctor extra'), ('Farmacia extra');
+insert into ClientTypes (name, hasProducts) values ('Doctor', 1), ('Farmacia', 0), ('Doctor extra', 1), ('Farmacia extra', 0);
 insert into Adoptions (name) values ('A'), ('B'), ('C');
 insert into Potentials (name) values ('Leal'), ('Probador'), ('Conocedor'), ('Usuario'); 
 
@@ -15,3 +15,10 @@ insert into Visits (FK_client, FK_reaction, comment, date, FK_visitType)
 	values (1, 1, 'Comment sample', GETDATE(), 1),
 		   (2, 3, 'Comment sample', GETDATE(), 2);
 insert into Products (name) values ('Acetaminofen'), ('Paracetamol');
+
+insert into Questions (question) values ('Calidad del servicio'), ('Atención del agente'), ('Satisfacción');
+
+insert into Options (description, FK_question) values 
+					('Excelente',1), ('Bueno',1), ('Regular',1), ('Malo',1),
+					('Excelente',2), ('Bueno',2), ('Regular',2), ('Malo',2),
+					('Excelente',3), ('Bueno',3), ('Regular',3), ('Malo',3)
